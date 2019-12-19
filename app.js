af78ff5fb4b2950746933806e61ffc9841faed60
Lurang <qorrhaapfhd@naver.com>
2019-12-19 17:58:42 +0900

    jquery fixed [changing socket.io to websocket]

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const path = require ('path');
-const http = require ('http');
+const path = require('path');
+const http = require('http');
 
-const nunjucks = require ('nunjucks');
-const bodyParser = require ('body-parser');
-const express = require ('express');
-const session = require ('express-session');
-const MySQLStore = require ('express-mysql-session') (session);
-const cors = require ('cors');
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const cors = require('cors');
 // testing?
 //const elasticsearch = require('elasticsearch');
 
-const indexRouter = require ('./router/index');
-const adminRouter = require ('./router/admin');
-const userRouter = require ('./router/user');
-const boardRouter = require ('./router/board');
+const indexRouter = require('./router/index');
+const adminRouter = require('./router/admin');
+const userRouter = require('./router/user');
+const boardRouter = require('./router/board');
 
 //express
-const app = express ();
-app.use (bodyParser.urlencoded ({extended: false}));
-app.use (bodyParser.json ());
-app.use (express.static (path.join (__dirname, 'public')));
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 //db
 const options = {
@@ -31,56 +31,117 @@ const options = {
 };
 
 //session-mysql
-const sessionStore = new MySQLStore (options);
-app.use (
-  session ({
-    secret: 'key!#@abcdef',
-    resave: false,
-    saveUninitialized: false,
-    store: sessionStore,
-  })
-);
+const sessionStore = new MySQLStore(options);
+
+const sessionParser = session({
+  secret: 'key!#@abcdef',
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore,
+});
+app.use(sessionParser);
 
 //nunjucks
-nunjucks.configure ('views', {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
 });
-app.set ('view engine', 'html');
+app.set('view engine', 'html');
 
 //cors error
 //app.use(cors())
-app.use ((req, res, next) => {
+app.use((req, res, next) => {
   //res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader ('Access-Control-Allow-Origin', '*'); //*대신 specific domain
-  res.setHeader (
+  res.setHeader('Access-Control-Allow-Origin', '*'); //*대신 specific domain
+  res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE'
+    'GET, POST, PUT, PATCH, DELETE',
   );
-  res.setHeader (
+  res.setHeader(
     'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Credentials'
+    'Content-Type, Authorization, Credentials',
   );
-  next ();
+  next();
 });
 
 //checkAdminPermission
 const auth = (req, res, next) => {
   if (req.session.user && req.session.user.admin) {
-    return next ();
+    return next();
   }
-  res.redirect ('/');
+  res.redirect('/');
 };
 //routes
-app.use (indexRouter.routes);
-app.use ('/user', userRouter.routes);
-app.use ('/admin', auth, adminRouter.routes);
-app.use ('/board', boardRouter.routes);
+app.use(indexRouter.routes);
+app.use('/user', userRouter.routes);
+app.use('/admin', auth, adminRouter.routes);
+app.use('/board', boardRouter.routes);
 
-//socket.io
+//websocket
+const server = http.createServer(app);
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+  verifyClient: (info, done) => {
+    sessionParser(info.req, {}, () => {
+      done(info.req.session);
+    })
+  },
+  server
+});
+
+wss.on('connection', (ws, req) => {
+  let msg = {};
+  console.log(req.session.user)
+  //message handler
+  ws.on('message', (message) => {
+    message = JSON.parse(message);
+    //message별로 행동
+    switch (message.event) {
+      //case login
+      case 'login':
+        ws.id = message.user;
+        if (!ws.id) {
+          ws.id = 'unknown';
+        }
+        msg = {
+          event: 'login',
+          id: ws.id,
+        }
+        //broadcast
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(msg));
+          }
+        })
+        break;
+      //case chat
+      case 'chat':
+        msg = {
+          event: 'chat',
+          msg: message.msg,
+          id: ws.id,
+        }
+        //broadcast
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(msg));
+          }
+        })
+        break;
+    }
+  });
+  
+  ws.on('close', (data) => {
+    
+  })
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+});
+
+
+/* Socket.IO
 const server = http.createServer (app);
 const io = require ('socket.io') (server);
-server.listen (3000); //app.listen(3000);
 
 io.on ('connection', socket => {
   socket.on ('disconnect', () => {
@@ -105,11 +166,13 @@ io.on ('connection', socket => {
     });
   });
 });
-
-/*
-    io.emit => 접속된 모든클라이언트에게 전송
-    socket.emit => 전송한 클라이언트에게만 전송
-    socket.broadcast.emit => 메시지를 전송한 클라이언트를 제외한 모든클라이언트
-                말그대로 브로드캐스트
-    io.to(id).emit 특정클라이언트에게만 메시지, id는 socket객체의 id속성가ㅄ;
 */
+/*
+  io.emit => 접속된 모든클라이언트에게 전송
+  socket.emit => 전송한 클라이언트에게만 전송
+  socket.broadcast.emit => 메시지를 전송한 클라이언트를 제외한 모든클라이언트
+              말그대로 브로드캐스트
+  io.to(id).emit 특정클라이언트에게만 메시지, id는 socket객체의 id속성값;
+*/
+
+server.listen (3000); //app.listen(3000);
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
-const gulp = require ('gulp');
-const browserSync = require ('browser-sync').create ();
-const nodemon = require ('gulp-nodemon');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon');
 
-gulp.task ('nodemon', callback => {
-  const stream = nodemon ({
-    script: 'app.js',
-    ext: 'js html css',
-    done: callback,
-  });
-  stream
-    .on ('restart', () => {
-      browserSync.reload ();
-    })
-    .on ('crash', () => {
-      console.error ('Application has crashed!\n');
-      //stream.emit('restart', 10)  // restart the server in 10 seconds
+gulp.task('nodemon', callback => {
+    const stream = nodemon({
+        script: 'app.js',
+        ext: 'js html css',
+        done: callback,
+    });
+    stream
+        .on('restart', () => {
+            browserSync.reload();
+        })
+        .on('crash', () => {
+            console.error('Application has crashed!\n');
+            //stream.emit('restart', 10)  // restart the server in 10 seconds
+        });
+});
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        proxy: 'http://localhost:3000',
+        port: 5000,
     });
 });
 
-gulp.task ('browser-sync', () => {
-  browserSync.init ({
-    proxy: 'http://localhost:3000',
-    port: 5000,
-  });
-});
-
-gulp.task ('default', gulp.parallel ('browser-sync', 'nodemon'));
+gulp.task('default', gulp.parallel('browser-sync', 'nodemon'));
